047c6781f2541a7041c810777ad653003a4116c4
your_name_here <mk@MKs-MacBook-Pro.local>
2019-08-19 19:00:02 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoHeader></TodoHeader> 
+    <TodoInput></TodoInput>  
+    <TodoList></TodoList>  
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoFooter from './components/TodoFooter'
+import TodoHeader from './components/TodoHeader'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TodoFooter,
+    TodoHeader,
+    TodoInput,
+    TodoList
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #f6f6f6;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.3);
 }
 </style>
---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="clearAllContainer">
+    <span class="clearAllButton" @click="clearTodo">Clear All</span>
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        clearTodo(){
+            localStorage.clear();
+        }
+    }
+};
+</script>
+
+<style scoped>
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+.clearAllButton {
+  color: #e30303;
+  display: block;
+}
+</style>
